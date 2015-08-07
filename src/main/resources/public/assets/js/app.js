11e0e45001d0e14203c075a189d55ccea6dda5d0
gryglbrt <gjgilbert@gmail.com>
2015-08-07 13:50:24 -0400

    UI notifications and loading gif

--- src/main/resources/public/assets/js/app.js
+++ src/main/resources/public/assets/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-.module('LAP', ['ui.bootstrap', 'ui.router', 'pascalprecht.translate', 'ui-notification']);
+.module('LAP', ['ui.bootstrap', 'ui.router', 'ngCookies', 'pascalprecht.translate', 'ui-notification']);
 
 angular
 .module('LAP')
@@ -22,6 +22,125 @@ angular
       });
 
     $translateProvider.preferredLanguage('en_us');
+})
+.provider('requestNotification', function() {
+	// This is where we keep subscribed listeners
+	var onRequestStartedListeners = [];
+	var onRequestEndedListeners = [];
+
+	// This is a utility to easily increment the request count
+	var count = 0;
+	var requestCounter = {
+		increment : function() {
+			count++;
+		},
+		decrement : function() {
+			if (count > 0)
+				count--;
+		},
+		getCount : function() {
+			return count;
+		}
+	};
+	// Subscribe to be notified when request starts
+	this.subscribeOnRequestStarted = function(listener) {
+		onRequestStartedListeners.push(listener);
+	};
+
+	// Tell the provider, that the request has started.
+	this.fireRequestStarted = function(request) {
+		// Increment the request count
+		requestCounter.increment();
+		// run each subscribed listener
+		angular.forEach(onRequestStartedListeners, function(listener) {
+			// call the listener with request argument
+			listener(request);
+		});
+		return request;
+	};
+
+	// this is a complete analogy to the Request START
+	this.subscribeOnRequestEnded = function(listener) {
+		onRequestEndedListeners.push(listener);
+	};
+
+	this.fireRequestEnded = function() {
+		requestCounter.decrement();
+		var passedArgs = arguments;
+		angular.forEach(onRequestEndedListeners, function(listener) {
+			listener.apply(this, passedArgs);
+		});
+		return arguments[0];
+	};
+
+	this.getRequestCount = requestCounter.getCount;
+
+	// This will be returned as a service
+	this.$get = function() {
+		var that = this;
+		// just pass all the
+		return {
+			subscribeOnRequestStarted : that.subscribeOnRequestStarted,
+			subscribeOnRequestEnded : that.subscribeOnRequestEnded,
+			fireRequestEnded : that.fireRequestEnded,
+			fireRequestStarted : that.fireRequestStarted,
+			getRequestCount : that.getRequestCount
+		};
+	};
+})
+.config(function($httpProvider, requestNotificationProvider) {
+	$httpProvider.defaults.transformRequest.push(function(data) {
+		requestNotificationProvider.fireRequestStarted(data);
+		return data;
+	});
+
+	$httpProvider.defaults.transformResponse.push(function(data) {
+		requestNotificationProvider.fireRequestEnded(data);
+		return data;
+	});
+})
+.config(function($httpProvider){
+	
+	var logsOutUserOn401 = ['$q','$location','$log','$cookieStore',
+		function($q, $location, $log, $cookieStore) {
+			var success = function(response) {
+				return response;
+			};
+	
+			var error = function(response) {
+				$log.log('error response');
+				$log.log(response)
+				if (response.status === 401) {
+	
+					var requestedurl = $location.path();
+	
+					if (requestedurl != '/') {
+						$cookieStore.put('origurl',requestedurl);
+						$log.info("Set origurl cookie to: "	+ $cookieStore.get('origurl'));
+					}
+	
+					$log.info('401 http status code - user should have been routed to /, requested url is: '+ requestedurl);
+					// redirect them back to login page
+					$location.path('/login');
+	
+					return $q.reject(response);
+				} else {
+					return $q.reject(response);
+				}
+			};
+	
+			return function(promise) {
+				return promise.then(success, error);
+			};
+		}];
+	
+	$httpProvider.interceptors.push(logsOutUserOn401);
+
+	if (!$httpProvider.defaults.headers.get) {
+		$httpProvider.defaults.headers.get = {};
+	}
+	$httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+	$httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 })
 .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
